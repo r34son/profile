2cbfc0b592bb4a9d900fc5d0262b86d3fb00d4b0
r34son <seitasanov.yahub@gmail.com>
2023-10-22 00:49:17 +0300

    chore: added dockerfile

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
