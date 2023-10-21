c4997bba647aa3dd964034aee9beafbd5c13d2a5
Yahub Seitasanov <seitasanov.yahub@gmail.com>
2023-10-22 00:18:40 +0300

    chore: Initial commit

---
+++ next.config.js
@@ -0,0 +1,16 @@
+const withPlugins = require('next-compose-plugins');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+module.exports = withPlugins([[withBundleAnalyzer]], nextConfig);
