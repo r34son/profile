536b4263c6712dce55ce8d747f8e4a4dcb06eef9
Iakhub Seitasanov <seitasanov.yahub@gmail.com>
2023-11-20 01:49:32 +0300

    feat: added screnshot tests on ci (#28)

--- jest.config.mjs
+++ jest.config.mjs
@@ -9,8 +9,8 @@ const createJestConfig = nextJest({
 /** @type {import('jest').Config} */
 const config = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-
   testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['/tests/'],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
