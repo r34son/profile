a8d9a99757db8d455214f7c629fc5a1bb097d144
r34son <seitasanov.yahub@gmail.com>
2023-12-15 23:22:40 +0300

    ci: updated node to lts and local setup for lhci

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,5 +1,14 @@
+const puppeteer = require('puppeteer');
+const isCI = require('is-ci');
+
 module.exports = {
   ci: {
+    collect: {
+      startServerCommand: 'pnpm lhci:start',
+      url: ['http://localhost:3000'],
+      numberOfRuns: 3,
+      chromePath: puppeteer.executablePath(),
+    },
     assert: {
       preset: 'lighthouse:no-pwa',
       assertions: {
@@ -11,7 +20,9 @@ module.exports = {
         'no-unload-listeners': 'off',
         'unused-css-rules': 'off',
         'unused-javascript': 'off',
+        'errors-in-console': isCI ? ['error', { minScore: 0.9 }] : 'off',
       },
     },
+    upload: { target: 'temporary-public-storage' },
   },
 };
