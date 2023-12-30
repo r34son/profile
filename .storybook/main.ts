ae6da37531a07a1bc75ccbe4a6e7f2806b45cce5
r34son <seitasanov.yahub@gmail.com>
2023-12-30 05:38:12 +0300

    feat: added storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,24 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.tsx'],
+  logLevel: 'error',
+  // staticDirs: ['../public'],
+  typescript: { reactDocgen: false, check: false },
+  core: { disableTelemetry: true },
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-themes',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: { builder: { useSWC: true } },
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+};
+
+export default config;
---
+++ .storybook/preview.tsx
@@ -0,0 +1,34 @@
+import type { Preview, ReactRenderer } from '@storybook/react';
+import { withThemeByClassName } from '@storybook/addon-themes';
+import { NextIntlClientProvider } from 'next-intl';
+import englishLocale from '@/messages/en.json';
+import '../src/app/globals.css';
+
+const preview: Preview = {
+  parameters: {
+    nextjs: { appDirectory: true },
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <NextIntlClientProvider locale="en" messages={englishLocale}>
+        <Story />
+      </NextIntlClientProvider>
+    ),
+    withThemeByClassName<ReactRenderer>({
+      themes: {
+        light: 'light',
+        dark: 'dark',
+      },
+      defaultTheme: 'light',
+    }),
+  ],
+};
+
+export default preview;
