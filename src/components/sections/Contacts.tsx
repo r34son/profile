ab5e3e83c62770a60aa6e9695d6d17d61b4842be
r34son <seitasanov.yahub@gmail.com>
2024-01-18 22:25:32 +0300

    feat: added contacts and updated deps

--- src/components/sections/Contacts.tsx
+++ src/components/sections/Contacts.tsx
@@ -1,10 +1,14 @@
 import { useTranslations } from 'next-intl';
+import { Contacts as ContactsList } from '@/components/Contacts';
 
 export const Contacts = () => {
-  const t = useTranslations('metadata');
+  const t = useTranslations('sections.contacts');
   return (
     <section id="contacts" className="h-96">
-      <h1>{t('title')}</h1>
+      <h2 className="mb-8 max-w-none text-center text-xl">{t('heading')}</h2>
+      <div className="flex justify-center">
+        <ContactsList />
+      </div>
     </section>
   );
 };
--- src/components/sections/Info.tsx
+++ src/components/sections/Info.tsx
@@ -1,7 +1,16 @@
 import { useTranslations } from 'next-intl';
 import { MoveRight } from 'lucide-react';
 import { Button } from '@/components/ui/button';
-import { Link } from '@/navigation';
+import {
+  DrawerTrigger,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerFooter,
+  DrawerClose,
+  Drawer,
+} from '@/components/ui/drawer';
+import { Contacts } from '@/components/Contacts';
 
 export const Info = () => {
   const t = useTranslations('sections.info');
@@ -23,15 +32,33 @@ export const Info = () => {
       <div className="mt-24 flex items-center justify-center">
         <div className="relative">
           <div className="absolute -inset-1 rounded-lg bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 opacity-75 blur"></div>
-          <Button
-            className="relative h-14 rounded-lg px-7 py-4 text-base dark:bg-black dark:text-white dark:hover:bg-black/75"
-            size="lg"
-            asChild
-          >
-            <Link href="#contacts">
-              {t('callToAction')} <MoveRight className="ml-2 h-4 w-4" />
-            </Link>
-          </Button>
+          <Drawer>
+            <DrawerTrigger asChild>
+              <Button
+                className="relative h-14 rounded-lg px-7 py-4 text-base dark:bg-black dark:text-white dark:hover:bg-black/75"
+                size="lg"
+              >
+                {t('callToAction')} <MoveRight className="ml-2 h-4 w-4" />
+              </Button>
+            </DrawerTrigger>
+            <DrawerContent>
+              <div className="mx-auto w-full max-w-sm">
+                <DrawerHeader>
+                  <DrawerTitle className="text-center">
+                    {t('drawer.title')}
+                  </DrawerTitle>
+                </DrawerHeader>
+                <Contacts />
+                <DrawerFooter>
+                  <DrawerClose asChild>
+                    <Button variant="outline">
+                      {t('drawer.closeBtnText')}
+                    </Button>
+                  </DrawerClose>
+                </DrawerFooter>
+              </div>
+            </DrawerContent>
+          </Drawer>
         </div>
       </div>
     </section>
