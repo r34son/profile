c75723366c69d9686667713c3ff6b47e1b6f6029
r34son <seitasanov.yahub@gmail.com>
2024-02-04 15:15:31 +0300

    ci: move to new infra

--- deploy/modules/serverless/sa.tf
+++ deploy/modules/serverless/sa.tf
@@ -4,9 +4,35 @@ resource "yandex_iam_service_account" "deploy_sa" {
   folder_id   = var.folder_id
 }
 
-resource "yandex_resourcemanager_folder_iam_member" "this" {
+resource "github_actions_environment_secret" "DEPLOY_SA_ID" {
+  repository      = data.github_repository.this.name
+  environment     = var.environment
+  secret_name     = "DEPLOY_SA_ID"
+  plaintext_value = yandex_iam_service_account.deploy_sa.id
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "cr_pusher" {
+  folder_id = var.folder_id
+  role      = "container-registry.images.pusher"
+  member    = "serviceAccount:${yandex_iam_service_account.deploy_sa.id}"
+}
+
+# https://cloud.yandex.ru/ru/docs/troubleshooting/serverless-containers/known-issues/service-account-is-not-available
+resource "yandex_resourcemanager_folder_iam_member" "sa_user" {
+  folder_id = var.folder_id
+  role      = "iam.serviceAccounts.user"
+  member    = "serviceAccount:${yandex_iam_service_account.deploy_sa.id}"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "sls_editor" {
   folder_id = var.folder_id
   role      = "serverless.containers.editor"
+  member    = "serviceAccount:${yandex_iam_service_account.deploy_sa.id}"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "sls_admin" {
+  folder_id = var.folder_id
+  role      = "serverless-containers.admin"
   member    = "serviceAccount:${yandex_iam_service_account.deploy_sa.id}"
 }
 
