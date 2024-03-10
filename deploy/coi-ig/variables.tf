091a14e8200b9bf7d47abccce27b38ba063eb9bd
r34son <seitasanov.yahub@gmail.com>
2024-03-10 21:35:57 +0300

    ci: move to dedicated vm

---
+++ deploy/coi-ig/variables.tf
@@ -0,0 +1,22 @@
+variable "folder_id" {
+  description = <<EOF
+    (Optional) The ID of the Yandex Cloud Folder that the resources belongs to.
+  EOF
+  type        = string
+  default     = null
+}
+
+variable "image_url" {
+  description = "(Required) Docker image url."
+  type        = string
+}
+
+variable "vm_user" {
+  type    = string
+  default = "yc-user"
+}
+
+variable "ssh_key" {
+  type      = string
+  sensitive = true
+}
