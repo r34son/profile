6f41bc97ab11ab95dcc42ceba2f75e872e8382ea
r34son <seitasanov.yahub@gmail.com>
2024-04-11 22:34:24 +0300

    chore: removed useless comments

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,11 +1,7 @@
 import nextJest from 'next/jest.js';
 
-const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-});
+const createJestConfig = nextJest({ dir: './' });
 
-// Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const config = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
@@ -13,5 +9,4 @@ const config = {
   testPathIgnorePatterns: ['/tests/'],
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default createJestConfig(config);
