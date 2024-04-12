0d2fc4f8ab5a1a39a5e514025873ace3bd371a94
r34son <seitasanov.yahub@gmail.com>
2024-04-12 20:55:26 +0300

    chore: release 1.1.0
    Release-As: 1.1.0

--- src/instrumentation.node.ts
+++ src/instrumentation.node.ts
@@ -1,6 +1,12 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { Resource } from '@opentelemetry/resources';
-import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
+import {
+  SEMRESATTRS_HOST_ARCH,
+  SEMRESATTRS_OS_TYPE,
+  SEMRESATTRS_PROCESS_RUNTIME_VERSION,
+  SEMRESATTRS_SERVICE_NAME,
+} from '@opentelemetry/semantic-conventions';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import {
   SentrySpanProcessor,
   SentryPropagator,
@@ -9,7 +15,11 @@ import {
 const sdk = new NodeSDK({
   resource: new Resource({
     [SEMRESATTRS_SERVICE_NAME]: 'next-app',
+    [SEMRESATTRS_HOST_ARCH]: process.arch,
+    [SEMRESATTRS_PROCESS_RUNTIME_VERSION]: process.version,
+    [SEMRESATTRS_OS_TYPE]: process.platform,
   }),
+  instrumentations: [getNodeAutoInstrumentations()],
   spanProcessors: [new SentrySpanProcessor()],
   textMapPropagator: new SentryPropagator(),
 });
