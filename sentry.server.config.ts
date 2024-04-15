55a51c356363bedd292038e9827eacd45a9c58a3
r34son <seitasanov.yahub@gmail.com>
2024-04-15 23:31:21 +0300

    feat: added sentry profiling

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,9 +1,12 @@
 import { init } from '@sentry/nextjs';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
 import { SENTRY_CAPTURE_RATE, SENTRY_DSN } from 'sentry.constants.mjs';
 
 init({
   dsn: SENTRY_DSN,
   tracesSampleRate: SENTRY_CAPTURE_RATE,
+  profilesSampleRate: SENTRY_CAPTURE_RATE,
   instrumenter: 'otel',
   environment: process.env.ENV,
+  integrations: [nodeProfilingIntegration()],
 });
