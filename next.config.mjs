d63f186e8a9022d3f2eaf0f687149fe87ea538e3
r34son <seitasanov.yahub@gmail.com>
2024-05-01 19:24:44 +0300

    feat: disable compression

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,9 @@ const nextConfig = {
   assetPrefix: process.env.ASSET_PREFIX,
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  compress: false,
   experimental: {
+    ppr: true,
     swcMinify: true,
     webpackBuildWorker: true,
     instrumentationHook: true,
