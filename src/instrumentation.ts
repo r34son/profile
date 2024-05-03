f6c9e11b9e51911918455d5f75a6c2359e32f5c6
r34son <seitasanov.yahub@gmail.com>
2024-05-03 23:50:48 +0300

    feat: use recommended sentry setup

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,8 +1,24 @@
+import { init } from '@sentry/nextjs';
+import { SENTRY_CAPTURE_RATE, SENTRY_DSN } from 'sentry.constants.mjs';
+
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('./instrumentation.node');
+    // https://github.com/getsentry/sentry-javascript/issues/11823
+    const { nodeProfilingIntegration } = await import('@sentry/profiling-node');
+    init({
+      dsn: SENTRY_DSN,
+      debug: false,
+      tracesSampleRate: SENTRY_CAPTURE_RATE,
+      profilesSampleRate: SENTRY_CAPTURE_RATE,
+      environment: process.env.ENV,
+      integrations: [nodeProfilingIntegration()],
+    });
   }
   if (process.env.NEXT_RUNTIME === 'edge') {
-    await import('./instrumentation.edge');
+    init({
+      dsn: SENTRY_DSN,
+      tracesSampleRate: SENTRY_CAPTURE_RATE,
+      environment: process.env.ENV,
+    });
   }
 }
