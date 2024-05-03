97dc20a55473f33b8222e68431fee4a6fa0e4b01
Iakhub Seitasanov <seitasanov.yahub@gmail.com>
2024-05-03 23:53:48 +0300

    feat: use recommended sentry setup (#485)

--- src/instrumentation.edge.ts
+++
@@ -1,8 +0,0 @@
-import { init } from '@sentry/nextjs';
-import { SENTRY_CAPTURE_RATE, SENTRY_DSN } from 'sentry.constants.mjs';
-
-init({
-  dsn: SENTRY_DSN,
-  tracesSampleRate: SENTRY_CAPTURE_RATE,
-  environment: process.env.ENV,
-});
