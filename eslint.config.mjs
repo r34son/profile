7f522cf36120bff1c8cdcf44bebfe0a813b7033a
Iakhub Seitasanov <seitasanov.yahub@gmail.com>
2024-06-23 01:56:22 +0300

    feat: update deps (#578)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,10 +13,35 @@ const compat = new FlatCompat({
 });
 const gitignorePath = path.resolve(__dirname, '.gitignore');
 
+/** @type {import("eslint").Linter.FlatConfig[]} */
 // eslint-disable-next-line import/no-anonymous-default-export
 export default [
   includeIgnoreFile(gitignorePath),
   ...fixupConfigRules(
     compat.extends('next/core-web-vitals', 'plugin:storybook/recommended'),
   ),
+  {
+    // 'https://next-intl-docs.vercel.app/docs/workflows/linting'
+    name: 'next-intl navigation',
+    rules: {
+      // Consistently import navigation APIs from `@/navigation`
+      'no-restricted-imports': [
+        'error',
+        {
+          name: 'next/link',
+          message: 'Please import from `@/navigation` instead.',
+        },
+        {
+          name: 'next/navigation',
+          importNames: [
+            'redirect',
+            'permanentRedirect',
+            'useRouter',
+            'usePathname',
+          ],
+          message: 'Please import from `@/navigation` instead.',
+        },
+      ],
+    },
+  },
 ];
